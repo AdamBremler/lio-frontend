2a54b3a20327e33958c74d7ec624caf9348df971
Adam Bremler <adambremler@hotmail.com>
2019-04-07 02:09:02 +0200

    Add router, add home and login components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div>
+            <Router>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+            </Router>
+        </div>
+    );
 }
-
-export default App;
