76d8fef0ef7ba2223c02fa06c92603e972dc12aa
Adam Bremler <adambremler@hotmail.com>
2019-04-09 02:48:50 +0200

    Add validation for register form, add InputField component

--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import InputField from '../common/InputField';
 
-function RegisterForm({ handleSubmit }) {
+const validate = values => {
+    const errors = {};
+
+    if (!values.name) {
+        errors.name = 'Required';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+
+    if (!values.password) {
+        errors.password = 'Required'
+    }
+
+    return errors;
+}
+
+function RegisterForm({ handleSubmit, pristine, reset, submitting }) {
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <Field name="email" component="input" type="email" />
-                </div>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <Field name="name" component="input" type="text" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <Field name="password" component="input" type="password" />
-                </div>
-                <button type="submit">Register</button>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field name="email" component={InputField} type="email" label="Email" />
+            </div>
+            <div>
+                <Field name="name" component={InputField} type="text" label="Name" />
+            </div>
+            <div>
+                <Field name="password" component={InputField} type="password" label="Password" />
+            </div>
+            <button type="submit" disabled={submitting}>Register</button>
+        </form>
     )
 }
 
 export default reduxForm({
-    form: 'register'
+    form: 'register',
+    validate
 })(RegisterForm);
---
+++ src/components/common/InputField.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function InputField({
+    input,
+    label,
+    type,
+    meta: { touched, error, warning }
+}) {
+    return (
+        <div>
+            <label>{label}</label>
+            <div>
+                <input {...input} placeholder={label} type={type} />
+                {touched && (
+                    (error && <span>{error}</span>) ||
+                    (warning && <span>{warning}</span>)
+                )}
+            </div>
+        </div>
+    )
+}
