ec8b144602bb89a8644676ef65398986fa858d8c
Adam Bremler <adambremler@hotmail.com>
2019-04-10 02:30:45 +0200

    Add localStorage helper for automatically saving user data

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,26 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import throttle from 'lodash/throttle';
 import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
+import { loadState, saveState } from '../helpers/localStorage';
+
+const middleware = [thunk];
+
+const persistedState = loadState();
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    persistedState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
+
+store.subscribe(throttle(() => {
+    saveState({
+        user: store.getState().user
+    });
+}, 1000));
 
 export default store;
