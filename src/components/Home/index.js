b4ec6ff674cfb33faaff1778ef93d8d94b8ee6ce
Tolga <tolga.koyluoglu@chasacademy.se>
2019-05-19 13:39:24 +0200

    Add styled with css for Home component.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Div = styled.div`
-        flex-grow: 1;
-`;
+import Div from './styled/Div';
+import Banner from './styled/Banner';
+import BannerText from './styled/BannerText';
+import Paragraph from './styled/Paragraph';
+import Header from './styled/Header';
+import Button from './styled/Button';
 
 export default function index() {
     return (
         <Div>
-            <h1>Home</h1>
-            <h4>Carousel Here ?</h4>
+            <Banner>
+                <BannerText>
+                    <Header>A Simpler way to find internship!</Header>
+                    <Paragraph>Lio is a platform for students and companys to connect with eachother. Sign up and create your profile today.</Paragraph>
+                    <Button>Join now!</Button>
+                </BannerText>
+            </Banner>
             <h4>Some info about the company here ?</h4>
         </Div>
     )
