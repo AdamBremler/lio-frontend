7e7255f883e3970c6a2ff258f31da080ae045282
Adam Bremler <adambremler@hotmail.com>
2019-05-21 15:40:01 +0200

    Include filters on initial feed submit

--- src/components/Feed/Filter/Search/Options/index.js
+++ src/components/Feed/Filter/Search/Options/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { submit } from 'redux-form';
 import options from './img/options.svg';
 import OptionsButtonWrapper from './styled/OptionsButtonWrapper';
 import OptionsButtonField from './styled/OptionsButtonField';
 
-export default function index({ dispatch, filterButtonValue }) {
+export default function index({ filterButtonValue, submitFeedForm }) {
     return (
-        <OptionsButtonWrapper title='Filter' onClick={() => filterButtonValue && dispatch(submit('feedSearch'))}>
+        <OptionsButtonWrapper title='Filter' onClick={filterButtonValue ? submitFeedForm : () => null}>
             <OptionsButtonField name='filterButton' component='input' type='checkbox' label='Search' />
             <img src={options} />
         </OptionsButtonWrapper>
