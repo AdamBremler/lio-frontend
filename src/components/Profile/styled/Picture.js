c630c42fb92fabc62e5dd62c561cd6fa1f89ca8a
tolgakoyluoglu <tolga.koyluoglu@chasacademy.se>
2019-05-22 16:13:28 +0200

    Add css for images.

--- src/components/Profile/styled/Picture.js
+++ src/components/Profile/styled/Picture.js
@@ -4,6 +4,8 @@ export default styled.img`
     border-radius: 100%;
     justify-content: center;
     display: flex;
+    width: 200px;
+    height: 200px;
     border: 1px solid #f3f3f3;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,.2), 0 3px 10px 0 rgba(0,0,0,.19);
 `;
