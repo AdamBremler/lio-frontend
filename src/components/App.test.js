cc5a3c333d3985e3c31c01f2346ed22bfa59a45f
Adam Bremler <adambremler@hotmail.com>
2019-05-30 01:44:37 +0200

    Fix app test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { ThemeProvider } from 'styled-components';
+import theme from '../themes/default';
+import store, { history } from '../stores';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <ThemeProvider theme={theme}>
+                    <App />
+                </ThemeProvider>
+            </ConnectedRouter>
+        </Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
